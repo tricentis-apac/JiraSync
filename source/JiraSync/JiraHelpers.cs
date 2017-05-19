94642913f1c75644febb809d6ee8adbca42fcbe3
David Colwell <d.colwell@tricentis.com>
2017-05-19 17:22:04 +1000

    I can describe this commit using this image https://memegenerator.net/instance/63124974/game-of-thrones-shame-shame
    On a side note, defects can now be committed to Jira

--- source/JiraSync/JiraHelpers.cs
+++ source/JiraSync/JiraHelpers.cs
@@ -27,57 +27,65 @@ namespace JiraSync
         public static async Task<JiraDefect> CreateIssue(String username, String password, String url, String project, String summary, String description, String issueType = "Bug")
         {
             JiraIssue jIssue = new JiraIssue();
-            jIssue.summary = summary;
-            jIssue.description = description;
-            jIssue.project.Add("project", project);
-            jIssue.issueType.Add("name", issueType);
+            string issueEndpoint;
+            jIssue.fields.summary = summary;
+            jIssue.fields.description = description;
+            jIssue.fields.project = new Project { key = project };
+            jIssue.fields.issuetype = new IssueType { name = issueType };
+            if (!url.Contains("/issue"))
+            {
+                string baseURL = url.Split(new char[] { '/' }).Take(3).Aggregate((x, y) => $"{x}/{y}");
+                issueEndpoint = $"{baseURL}/rest/api/latest/issue";
+            }
+            else
+                issueEndpoint = url;
 
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(url);
+            client.BaseAddress = new Uri(issueEndpoint);
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password)));
             //client.DefaultRequestHeaders.Add("Content-Type", "application/json");
 
-            StringContent payload = new StringContent(JsonConvert.SerializeObject(jIssue), Encoding.UTF8, "application/json");
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
-            request.Content = payload;
+            var serializerSettigns = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
 
-            //using (HttpResponseMessage resp = await client.PostAsync(url, request).ConfigureAwait(false))
-
-           // using (HttpResponseMessage response = await client.PostAsync(url, request).ConfigureAwait(false))
-
-
-                //    HttpResponseMessage response;
-                //client.SendAsync(request).ContinueWith(responseTask =>
-                //    {
-                //        response = responseTask.Result;
-                //    });
+            StringContent payload = new StringContent(JsonConvert.SerializeObject(jIssue, Newtonsoft.Json.Formatting.None,serializerSettigns), Encoding.UTF8, "application/json");
+            //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
+            //request.Content = payload;
 
 
-                return new JiraDefect("", "", "");
+            HttpResponseMessage response = await client.PostAsync(client.BaseAddress, payload);
+            JiraDefect defectPlaceholder = JsonConvert.DeserializeObject<JiraDefect>(response.Content.ReadAsStringAsync().Result);
+            return defectPlaceholder;
         }
 
         public class JiraDefect
         {
             public String ID;
             public String Key;
-            public String URL;
+            public String self;
 
-            public JiraDefect(String id, String key, String url)
+            public JiraDefect(String id, String key, String self)
             {
                 ID = id;
                 Key = key;
-                URL = url;
+                this.self = self;
             }
         }
 
         public class JiraIssue
         {
-            public Dictionary<String, String> project = new Dictionary<string, string>();
-            public Dictionary<String, String> issueType = new Dictionary<string, string>();
+            //public Dictionary<String, String> project = new Dictionary<string, string>();
+            //public Dictionary<String, String> issueType = new Dictionary<string, string>();
+            public JiraIssue()
+            {
+                fields = new JiraFields();
+            }
 
-            public String summary;
-            public String description;
+            public JiraFields fields { get; set; }
         }
+
     }
 }
