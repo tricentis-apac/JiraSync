3907a59bb44848091792a31b1301d3e81fc0e815
dbcol <dbcol@DESKTOP-3RUT3U9>
2017-09-15 09:24:44 +1000

    Jira commit

---
+++ source/JiraService/Configuration/TestSyncDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraService.Configuration
+{
+    public class TestSyncDefinition
+    {
+        public string testPlaceholderTypeName { get; set; }
+        public string jiraStatusField { get; set; }
+    }
+}
